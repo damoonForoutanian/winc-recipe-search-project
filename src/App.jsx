1e1698b3dd1ff1512d42b65e999300ef1eef850e
damoonForoutanian <d.foroutanian@gmail.com>
2024-09-08 01:34:49 +0200

    Create App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import { RecipeListPage } from './pages/RecipeListPage';
+import { RecipeItemPage } from './pages/RecipeItemPage';
 
 export const App = () => {
-  // Your state code here
-  return <RecipeListPage />;
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
+
+  return selectedRecipe ? (
+    <RecipeItemPage
+      recipe={selectedRecipe}
+      onBack={() => setSelectedRecipe(null)}
+    />
+  ) : (
+    <RecipeListPage onRecipeClick={(recipe) => setSelectedRecipe(recipe)} />
+  );
 };
