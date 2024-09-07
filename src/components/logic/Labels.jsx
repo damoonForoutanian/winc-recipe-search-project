dc33297f4efc314e5e0b290a5b60f8983788c87e
damoonForoutanian <d.foroutanian@gmail.com>
2024-09-08 01:29:01 +0200

    Create Labels logic component

---
+++ src/components/logic/Labels.jsx
@@ -0,0 +1,33 @@
+import { Box, Text } from '@chakra-ui/react';
+
+export const Labels = ({ labelType, labels, color, filter }) => {
+  if (!labels) return null;
+
+  const filteredLabels = filter
+    ? Object.keys(labels)
+        .filter((key) => filter.includes(key))
+        .map((key) => labels[key])
+    : labels;
+
+  return (
+    <Box mt={2}>
+      {labelType && <Text fontWeight='bold'>{labelType}:</Text>}
+      {filteredLabels.length > 0 ? (
+        filteredLabels.map((label, index) => (
+          <Text
+            key={index}
+            display='inline-block'
+            bg={color}
+            p={1}
+            borderRadius='md'
+            m={1}
+          >
+            {label.label || label}
+          </Text>
+        ))
+      ) : (
+        <Text>No {labelType.toLowerCase()} available</Text>
+      )}
+    </Box>
+  );
+};
