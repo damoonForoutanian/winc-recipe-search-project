856b81cb70e13b2f097d44b2ad968fb26ed44c68
damoonForoutanian <d.foroutanian@gmail.com>
2024-09-08 02:29:06 +0200

    Change SearchBar look

--- src/components/logic/SearchBar.jsx
+++ src/components/logic/SearchBar.jsx
@@ -1,13 +1,18 @@
 import { Input } from '@chakra-ui/react';
 
-export const SearchBar = ({ placeholder, value, onChange }) => {
+export const SearchBar = ({ value, onChange }) => {
   return (
     <Input
-      placeholder={placeholder}
       value={value}
       onChange={onChange}
+      placeholder='Search recipes'
+      _placeholder={{ color: '#2563DA' }}
       size='lg'
       maxW='610px'
+      bg='#F9F6EE'
+      color='#7299E6'
+      border='none'
+      borderRadius='md'
     />
   );
 };
