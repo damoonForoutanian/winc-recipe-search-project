3fb0faa35496b0f5b49cc4703a37a8021dffe6b9
damoonForoutanian <d.foroutanian@gmail.com>
2024-09-08 03:35:03 +0200

    Tweaked Labels

--- src/components/logic/Labels.jsx
+++ src/components/logic/Labels.jsx
@@ -1,3 +1,37 @@
+// import { Box, Text } from '@chakra-ui/react';
+
+// export const Labels = ({ labelType, labels, color, filter }) => {
+//   if (!labels) return null;
+
+//   const filteredLabels = filter
+//     ? Object.keys(labels)
+//         .filter((key) => filter.includes(key))
+//         .map((key) => labels[key])
+//     : labels;
+
+//   return (
+//     <Box mt={2}>
+//       {labelType && <Text fontWeight='bold'>{labelType}:</Text>}
+//       {filteredLabels.length > 0 ? (
+//         filteredLabels.map((label, index) => (
+//           <Text
+//             key={index}
+//             display='inline-block'
+//             bg={color}
+//             p={1}
+//             borderRadius='md'
+//             m={1}
+//           >
+//             {label.label || label}
+//           </Text>
+//         ))
+//       ) : (
+//         <Text>No {labelType.toLowerCase()} available</Text>
+//       )}
+//     </Box>
+//   );
+// };
+
 import { Box, Text } from '@chakra-ui/react';
 
 export const Labels = ({ labelType, labels, color, filter }) => {
@@ -11,8 +45,10 @@ export const Labels = ({ labelType, labels, color, filter }) => {
 
   return (
     <Box mt={2}>
-      {labelType && <Text fontWeight='bold'>{labelType}:</Text>}
-      {filteredLabels.length > 0 ? (
+      {labelType && filteredLabels.length > 0 && (
+        <Text fontWeight='bold'>{labelType}:</Text>
+      )}
+      {filteredLabels.length > 0 &&
         filteredLabels.map((label, index) => (
           <Text
             key={index}
@@ -24,10 +60,7 @@ export const Labels = ({ labelType, labels, color, filter }) => {
           >
             {label.label || label}
           </Text>
-        ))
-      ) : (
-        <Text>No {labelType.toLowerCase()} available</Text>
-      )}
+        ))}
     </Box>
   );
 };
